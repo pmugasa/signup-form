4c5d6ea0bf11d0275e4e04f99b4cb41bcc9a7b74
Pete Laptop <pmugasa78@gmail.com>
2022-10-24 23:47:14 +0200

    added the css form validation

---
+++ src/index.js
@@ -0,0 +1,3 @@
+// get  the values of the pwd inputs
+// if the two inputs match then insert a green border line
+// if the two inputs do not match insert a red border line and a text saying that pwds should match
